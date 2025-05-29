2d434f0731bc53577e76bc03bf11b900d3c9d1b9
sway-sea <itraynes@gmail.com>
2025-05-29 13:23:29 +0200

    added sys.argv

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from stats import get_word_count
 from stats import char_count
 from stats import d_list
+import sys
 
 def main():
-    count = get_word_count("books/frankenstein.txt")
-    #text = get_book_text("books/frankenstein.txt")
-    #print(f"{count} words found in the document")
-    #char_count("books/frankenstein.txt")
-    d = char_count("books/frankenstein.txt")
-    x = d_list(d)
 
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {count} total words")
-    print("--------- Character Count -------")
-    for i in x:
-        if i["char"].isalpha():
-            print(f"{i["char"]}: {i["num"]}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        count = get_word_count(sys.argv[1])
+        d = char_count(sys.argv[1])
+        x = d_list(d)
+
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {count} total words")
+        print("--------- Character Count -------")
+        for i in x:
+            if i["char"].isalpha():
+                print(f"{i["char"]}: {i["num"]}")
+        print("============= END ===============")
     
 main()
