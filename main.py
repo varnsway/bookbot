142145a83709c5f77e772391dca3f276330020bc
sway-sea <itraynes@gmail.com>
2025-05-29 12:59:00 +0200

    added print logic

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ from stats import d_list
 def main():
     count = get_word_count("books/frankenstein.txt")
     #text = get_book_text("books/frankenstein.txt")
-    print(f"{count} words found in the document")
-    char_count("books/frankenstein.txt")
+    #print(f"{count} words found in the document")
+    #char_count("books/frankenstein.txt")
     d = char_count("books/frankenstein.txt")
     x = d_list(d)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
     for i in x:
         if i["char"].isalpha():
-            print(i)
+            print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
     
 main()
